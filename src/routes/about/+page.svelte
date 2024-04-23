1f311e7fe815deb997ba7d608694a4e3bd376cb4
Steve Lee <steve@opendirective.com>
2024-04-23 17:27:33 +0100

    Tweak about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -17,10 +17,16 @@
   major scale tonic.
 </p>
 <p>
-  The main use case is to confirm the correct syllable is being sung when used
-  in conjunction with a Pitch to MIDI converter such as <a
+  The main use case is to confirm the relative syllable or number matches what
+  is being played. You can even check what is being sung when used in
+  conjunction with a Pitch to MIDI converter such as <a
     href="https://vochlea.com/">Dubler</a
   >.
+</p>
+<p>
+  Note that when MIDI devices are connected or virtual midi programs launched
+  the browser page needs to be refreshed (F5) in order for them to become
+  available.
 </p>
 
 <p>
