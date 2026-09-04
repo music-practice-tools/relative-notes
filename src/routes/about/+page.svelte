9038f3553b9100d7ddb924c0591f9693560d32bd
Steve Lee <steve@fullmeasure.co.uk>
2026-09-04 20:54:21 +0100

    rework about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,10 +9,13 @@
     data-sveltekit-reload>Back to App</a>
 </p>
 <p>
-  <strong>Relative Notes</strong> is a web app for recognising the scale degree of a sung or MIDI note.</p>
-<p>A scale degrees is the musical distance or 'interval' from a refetence 'tonic' note (root) and so a relative naming scheme, compared to absolute note names like 'C'.
+  <strong>Relative Notes</strong> is a web app for recognising the scale degree of a sung note or a MIDI note on message.</p>
+<p>A scale degree (AKA scale function) is the musical distance or 'interval' of a note from a reference 'tonic' note (root) of a scale. This is a relative naming scheme, compared to absolute note names like 'C'. Scale degrees of single notes here are closely related to chord function as those are defined by the chord root note.
 </p>
-<p>This alows easy transposing between keys. It also aids ear training as the 'feel' of each degree can be learnt. The degrees can be displayed in Arabic or Roman numbers and solfege and Solfège or Sargam syllables.
+<p>Using scale degrees aids ear training as the 'feel' of each degree can be learnt. It also alows easy transposing between keys of a melody or harmony. 
+</p>
+<p>
+The app shows the currently detected scale degree as Arabic numbers, Roman numbers, Solfège or Sargam syllables.
 </p>
 <p>
   Here's a short <a href="https://www.youtube.com/watch?v=CsQQuxrUV9E)"
@@ -22,38 +25,50 @@
     >blog post</a> about it.
 </p>
 <p>
-  The app monitors notes sung into a microphone or MIDI 'noteon' messages coming
+  The app monitors notes sung into a microphone or MIDI 'noteon' messages
   from external or virtual MIDI devices. It then displays the appropriate
-  syllable or number for the nore relative to a tonic note. It also shows the
-  (major) scale functions on an <a href="https://improviseforreal.com/"
+  number or syllable relative to the selected tonic. It also shows the degree on an <a href="https://improviseforreal.com/"
     >Improvise for Real</a> tonal map diagram as well as some extra detail on the
-  input note. The current note can be set as the tonic and then future notes will
+  current note. The current note can be set as the tonic and future notes will
   be relative to that.
 </p>
 <h2>Sung Pitch recognition</h2>
 <p>
-  I originally created Relative-Notes due to the lack of tools that handle
+  I created Relative-Notes due to the lack of tools that handle
   relative pitch for ear training. Specifically, I wanted something to confirm
-  the scale functions I sang were as expected.
+ that the scale functions I sang were as expected.
 </p>
 <p>
-  There is a built in pitch converter that works fairly well. or your can use
+  The built in pitch converter works well or your can use
   something like the excellent <a href="https://vochlea.com/">Dubler</a> Pitch to
   MIDI converter as source.
 </p>
 <h2>Usage Notes</h2>
 <p>
-  You will need to enable microphone or MIDI access for the browser when asked.
-  In addition, when MIDI devices are connected or virtual midi programs launched
-  the browser will likely need to be refreshed (F5) or restarted in order for
-  them to become available snd work. Sometimes you may need to use browser
+You will need to enable microphone or MIDI access for the browser when asked.
+</p>
+<p>
+  In addition, when new microphones or MIDI devices are connected or virtual midi programs launched
+  the browser will likely need to be refreshed (F5) or even restarted in order for
+  them to become available for selection. Sometimes you may need to use browser
   controls to reset the mic or midi access so you can select another source.
 </p>
 <p>
-  When using such virtual MIDI devices (programs that generate MIDI) on Windows
-  you might need a virtual MIDI cable such as <a
-    href="https://www.tobias-erichsen.de/software/loopmidi.html">loopMidi</a
+A quiet room and good mic will give best results. some laptop mics can give poor results.
+</p>
+<p>
+  When using virtual MIDI devices (programs that generate MIDI) on Windows
+  you might need a virtual MIDI cable such as <a href="https://www.tobias-erichsen.de/software/loopmidi.html">loopMidi</a
   >. Maybe the upcoming MIDI 2.0 support in Windows will fix this?
+</p>
+<h2>Limitation</h2>
+<p>
+<ul>
+<li>Major scale only</li>
+<li>Non diatonic notes are shown as flat (b)</li>
+<li>MIDI is polyphonic and chords show notes in rapid succession as they are received. Microphone is monophonic.</li>
+<\ul>
+Major
 </p>
 <p>
   This is a Music Practice Tools <a
